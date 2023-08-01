121e2e1a80eebeebcc9449a86b1c20a2fb148c32
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-08-01 15:10:21 +0000

    02. Setup bootstrap and NavBar.js

---
+++ remember-app/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+
+const NavBar = () => {
+  return (
+    <div>
+        NavBar
+        <Button as="a" variant="primary">
+        Button as link
+        </Button>
+    </div>
+  )
+}
+
+export default NavBar
