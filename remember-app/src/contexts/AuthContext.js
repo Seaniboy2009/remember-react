8971dedb7c59937eef457eba34ce57afac16015b
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-09-18 21:24:41 +0000

    had to install jwt-decode again due to corruption

--- remember-app/src/contexts/AuthContext.js
+++ remember-app/src/contexts/AuthContext.js
@@ -1,7 +1,7 @@
 import { createContext, useState, useEffect } from "react";
-import jwt_decode from "jwt-decode"
 import { useNavigate } from 'react-router-dom';
-
+import jwt_decode from 'jwt_decode'
+    
 const AuthContext = createContext()
 
 export default AuthContext
